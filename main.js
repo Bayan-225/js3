3f92c562ab0157570edb6785d1a437a59a137d30
Bayan-225 <147585665+Bayan-225@users.noreply.github.com>
2023-12-11 11:58:51 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function sayHello(name){
+    console.log("Привет, " + name + "!")
+    document.writeln("Привет, " + name + "!")
+}
+sayHello("Yarik");
+
+function add(a, b){
+  
+    return a + b;
+}
+const result = add(3, 3);
+console.log(result)
+document.writeln(result)
+
+function add2(c, d){
+    return Math.pow(c, d);
+
+}
+const pow = add2(5, 3);
+console.log(pow)
+document.writeln(pow)
